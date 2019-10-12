1920a3afacf17d888991b5df292d6d6a53cee3c4
Eric Mellow <ericmellow@gmail.com>
2019-10-12 13:07:16 -0600

    Update cards with answers

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -4,13 +4,31 @@ import './Card.css';
 class Card extends Component {
   constructor(props) {
     super()
+    this.state = {
+      showAnswer: false,
+    }
+  }
+
+  showAnswer() {
+    this.setState({
+      showAnswer: !this.state.showAnswer
+    })
   }
   
   render() {
       return (
-        <div className='Card'>
+        <div className='Card' onClick={() => {
+          this.showAnswer()
+        }}>
           <h3 className='category'>{this.props.category}</h3>
-            <h4 className='question'>{this.props.question}</h4>
+          <h4 className='question'>{this.props.question}</h4>
+          {this.state.showAnswer ? 
+            <div>
+            <h4 className="answer">The correct answer is: {this.props.answer}</h4>
+            <h4>Did you guess the correct answer?</h4>
+            <button onClick={() => {
+            this.props.addPoint()}}>Yes</button>
+            </div> : null}
         </div>
       );
   }
