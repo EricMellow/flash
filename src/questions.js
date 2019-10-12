89472de1919a22e8afd57f64fb06566cd6bdfd02
Eric Mellow <ericmellow@gmail.com>
2019-10-12 13:04:02 -0600

    Add card data

---
+++ src/questions.js
@@ -0,0 +1,154 @@
+const data = [
+  {
+    "category": "Animals",
+    "question": "Ambergris is a waxy substance that originates as a secretion from what aquatic animal?",
+    "answer": "Sperm whale"
+  },
+  {
+    "category": "Animals",
+    "question": "What semi-aquatic, egg laying mammal, is native to Australia and Tasmania?",
+    "answer": "The platypus (Ornithorhynchus anatinus)"
+  },
+  {
+    "category": "Animals",
+    "question": "What is the term for a group of kangaroos?",
+    "answer": "Mob, troop or court"
+  },
+  {
+    "category": "Animals",
+    "question": "Which bird has eyes that are larger than its brain?",
+    "answer": "Ostrich"
+  },
+  {
+    "category": "Animals",
+    "question": "What is the only mammal born with horns?",
+    "answer": "Giraffe"
+  },
+  {
+    "category": "Computers",
+    "question": "What three-digit web error code for censored content is a reference to a Ray Bradbury novel?",
+    "answer": "451"
+  },
+  {
+    "category": "Computers",
+    "question": "1,024 Gigabytes is equal to one what?",
+    "answer": "Terabyte"
+  },
+  {
+    "category": "Computers",
+    "question": "When talking about computer memory, what does the acronym ROM stand for?",
+    "answer": "Read-only memory"
+  },
+  {
+    "category": "Computers",
+    "question": "In what year was the first Apple computer released?",
+    "answer": "1976"
+  },
+  {
+    "category": "Computers",
+    "question": "CERN launched the very first website in what year?",
+    "answer": "1990"
+  },
+  {
+    "category": "Earth",
+    "question": "Which is the most abundant metal in the earth’s crust?",
+    "answer": "Aluminum"
+  },
+  {
+    "category": "Earth",
+    "question": "What is the world’s largest ocean?",
+    "answer": "The Pacific Ocean"
+  },
+  {
+    "category": "Earth",
+    "question": "What is the name of the phenomenon when the magnetosphere in the Northern Hemisphere is disturbed by the solar wind resulting in a natural light display?",
+    "answer": "Aurora Borealis (Northern Lights)"
+  },
+  {
+    "category": "Earth",
+    "question": "What is the second most abundant element in the earth’s atmosphere?",
+    "answer": "Oxygen"
+  },
+  {
+    "category": "Earth",
+    "question": "In what month is the Earth closest to the sun?",
+    "answer": "January"
+  },
+  {
+    "category": "Geek",
+    "question": "The Punisher is a fictional character appearing in comic books published by which company?",
+    "answer": "Marvel Comics"
+  },
+  {
+    "category": "Geek",
+    "question": "In the movie The Terminator, what is the name of the company that created Skynet?",
+    "answer": "Cyberdyne Systems"
+  },
+  {
+    "category": "Geek",
+    "question": "What is the name of the American animated sci-fi sitcom about the misadventures of a mad scientist and his grandson?",
+    "answer": "Rick and Morty"
+  },
+  {
+    "category": "Geek",
+    "question": "What is the unit of length that is approximately 3.26 light-years?",
+    "answer": "Parsec"
+  },
+  {
+    "category": "Geek",
+    "question": "Star Trek: The Next Generation originally aired in what year?",
+    "answer": "1987"
+  },
+  {
+    "category": "Literature",
+    "question": "Mark Twain’s 1885 novel The Adventures of Huckleberry Finn was set along which river?",
+    "answer": "Mississippi River"
+  },
+  {
+    "category": "Literature",
+    "question": "In Nathaniel Hawthorne’s historical fition novel “The Scarlet Letter”, what was the letter?",
+    "answer": " 'A' stands for adulteress"
+  },
+  {
+    "category": "Literature",
+    "question": "Published in 2009, which American writer wrote the science fiction novel “Under the Dome”?",
+    "answer": "Stephen King"
+  },
+  {
+    "category": "Literature",
+    "question": "Victorian writers Charlotte, Emily, and Anne were sisters sharing what last name?",
+    "answer": "Brontë"
+  },
+  {
+    "category": "Literature",
+    "question": "Resistance to Civil Government (Civil Disobedience), is an 1849 essay written by which American author?",
+    "answer": "Henry David Thoreau"
+  },
+  {
+    "category": "Science",
+    "question": "How many planets in our solar system are not named after a god or goddess?",
+    "answer": "One, Earth"
+  },
+  {
+    "category": "Science",
+    "question": "On the periodic table of elements, which element has the shortest name?",
+    "answer": "Tin"
+  },
+  {
+    "category": "Science",
+    "question": "Menlo Park New Jersey was home to which inventors research laboratory?",
+    "answer": "Thomas Edison"
+  },
+  {
+    "category": "Science",
+    "question": "What is the term for the speed that a rocket needs to be traveling to break free of Earth’s gravity?",
+    "answer": "Escape Velocity"
+  },
+  {
+    "category": "Science",
+    "question": "The change in a wave’s frequency when the source and observer are in motion relative to one another is known as what?",
+    "answer": "The Doppler Effect (Doppler Shift)"
+  },
+]
+
+export default data;
