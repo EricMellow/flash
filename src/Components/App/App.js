fc1ad68cb60ecb6041bdd5532c72a812ee932506
Eric Mellow <ericmellow@gmail.com>
2019-10-12 08:35:36 -0600

    Update file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return(
+      <div className="Flash">
+        <h1>FLASH</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
