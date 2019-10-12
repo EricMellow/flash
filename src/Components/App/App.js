ebda7f38c6cddb44a4b654409c91e8ade8783023
Eric Mellow <ericmellow@gmail.com>
2019-10-12 13:05:51 -0600

    Add function to randomize cards

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,26 @@ import './App.css';
 
 class App extends Component {
 
+  getRandomCard() {
+    const cards = this.state.cardData.map((card, index) => {
+      return (
+        <Card
+          key={index}
+          category={card.category}
+          question={card.question}
+          answer={card.answer}
+          addPoint={this.addPoint}
+        />
+      );
+    });
+
+    const index = Math.floor(Math.random() * ((cards.length - 1) - 0 + 1)) + 0
+
+    return cards[index]
+  }
   render() {
+    const card = this.getRandomCard();
+
     return(
       <div className="Flash">
         <h1>FLASH</h1>
