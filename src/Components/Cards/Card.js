24668289350a116bb1d82eb82f3e6a3d8620f5fc
Eric Mellow <ericmellow@gmail.com>
2019-10-12 18:52:06 -0600

    Update card for question/answer

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -6,6 +6,8 @@ class Card extends Component {
     super()
     this.state = {
       showAnswer: false,
+      correctAnswers: 0,
+      timesSeen: 0
     }
   }
 
@@ -16,21 +18,53 @@ class Card extends Component {
   }
   
   render() {
+    if (!this.state.showAnswer) {
       return (
-        <div className='Card' onClick={() => {
+        <div 
+          className='Card' 
+          aria-label="Flash card"
+          onClick={() => {
           this.showAnswer()
         }}>
-          <h3 className='category'>{this.props.category}</h3>
-          <h4 className='question'>{this.props.question}</h4>
-          {this.state.showAnswer ? 
-            <div>
-            <h4 className="answer">The correct answer is: {this.props.answer}</h4>
-            <h4>Did you guess the correct answer?</h4>
-            <button onClick={() => {
-            this.props.addPoint()}}>Yes</button>
-            </div> : null}
+          <h2 className='category'>Category: {this.props.category}</h2>
+          <p>You have answered this question correctly {this.state.correctAnswers} times out of {this.state.timesSeen}</p>
+          <h4 className='question' aria-label="Question">{this.props.question}</h4>
+          <p>*Click card to see the answer</p>
         </div>
       );
+    } else {
+      return (
+        <div className='Card' aria-label="Flash card">
+          <h2 className="category">Answer</h2>
+          <h4 className="answer" aria-label="Answer">The correct answer is: {this.props.answer}</h4>
+          <h4>Did you guess the correct answer?</h4>
+          <button 
+            className="yes-btn" 
+            aria-label="Yes button"
+            onClick={() => {
+            let newScore = this.state.correctAnswers;
+            let timesSeen = this.state.timesSeen
+            this.setState({
+              showAnswer: false,
+              correctAnswers: newScore += 1,
+              timesSeen: timesSeen += 1
+            })
+            this.props.updateScore(1)
+          }}>Yes</button>
+          <button 
+            className="no-btn" 
+            aria-label="No button"
+            onClick={() => {
+            let timesSeen = this.state.timesSeen
+            this.setState({
+              showAnswer: false,
+              timesSeen: timesSeen += 1
+            })
+            this.props.updateScore(0)
+          }}>No</button>
+        </div>
+      );
+    }
   }
 }
 export default Card;
