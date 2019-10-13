525fd75df6be1f1a56331b0ff194bd4c336b4be5
Eric Mellow <ericmellow@gmail.com>
2019-10-13 14:57:18 -0600

    Add Sidebar tests

--- src/Components/Sidebar/Sidebar.test.js
+++ src/Components/Sidebar/Sidebar.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from './Sidebar';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Sidebar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter('enzyme-adapter-react-15') });
+
+describe('Sidebar', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Sidebar />)
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Sidebar />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+})
+
