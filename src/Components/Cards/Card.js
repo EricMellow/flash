b5e8753d31c5206ff73f61d2ee6f0b42eae9c6f5
Eric Mellow <ericmellow@gmail.com>
2019-10-13 19:16:41 -0600

    Update Card and styling

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -6,8 +6,6 @@ class Card extends Component {
     super()
     this.state = {
       showAnswer: false,
-      correctAnswers: 0,
-      timesSeen: 0
     }
   }
 
@@ -27,7 +25,6 @@ class Card extends Component {
           this.showAnswer()
         }}>
           <h2 className='category'>Category: {this.props.category}</h2>
-          <p>You have answered this question correctly {this.state.correctAnswers} times out of {this.state.timesSeen}</p>
           <h4 className='question' aria-label="Question">{this.props.question}</h4>
           <p>*Click card to see the answer</p>
         </div>
@@ -37,17 +34,13 @@ class Card extends Component {
         <div className='Card' aria-label="Flash card">
           <h2 className="category">Answer</h2>
           <h4 className="answer" aria-label="Answer">The correct answer is: {this.props.answer}</h4>
-          <h4>Did you guess the correct answer?</h4>
+          <p>Did you guess the correct answer?</p>
           <button 
             className="yes-btn" 
             aria-label="Yes button"
             onClick={() => {
-            let newScore = this.state.correctAnswers;
-            let timesSeen = this.state.timesSeen
             this.setState({
               showAnswer: false,
-              correctAnswers: newScore += 1,
-              timesSeen: timesSeen += 1
             })
             this.props.updateScore(1)
           }}>Yes</button>
@@ -55,10 +48,8 @@ class Card extends Component {
             className="no-btn" 
             aria-label="No button"
             onClick={() => {
-            let timesSeen = this.state.timesSeen
             this.setState({
               showAnswer: false,
-              timesSeen: timesSeen += 1
             })
             this.props.updateScore(0)
           }}>No</button>
